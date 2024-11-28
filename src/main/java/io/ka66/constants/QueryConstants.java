77d4ee33ff2ee7f83a277269b1228c414dc51b22
redx-tg47 <pgithub47@gmail.com>
2024-11-28 13:55:47 +0530

    Added QueryConstants and Db Properties

---
+++ src/main/java/io/ka66/constants/QueryConstants.java
@@ -0,0 +1,18 @@
+package io.ka66.constants;
+
+public class QueryConstants {
+
+  private QueryConstants() {
+  }
+
+  public static final String SELECT_FROM_DB_ORDER_BY_NAME = "" ;
+  public static final String SELECT_FROM_DB_WHERE_K_ID = "" ;
+  public static final String DELETE_FROM_DB_WHERE_K_ID = "" ;
+  public static final String UPDATE_DB = "" ;
+  public static final String SALARY_BETWEEN_MAX_AND_MINI = "" ;
+  public static final  String ID = "";
+  public static final  String NAME = "";
+  public static final  String SALARY = "";
+  public static final  String DEPARTMENT = "";
+
+}
